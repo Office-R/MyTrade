324be35e734f84c93b93e928e5c136c172754476
robin.frehner <robin.frehner@Hawaii-PC-03.sanschoolsbsdom.local>
2015-05-28 13:43:59 +0200

    SQL STATEMENTS

--- MyTrade/src/ch/zkb/mytrade/dao/Test.java
+++ MyTrade/src/ch/zkb/mytrade/dao/Test.java
@@ -16,6 +16,25 @@ public class Test {
 		}
 	}
 
+	
+	-----SQL STATEMENT FÜR OFFENE POSITIONEN ANZEIGEN------
+
+	SELECT auftrag.preis, aktie.name, aktie.aktie_id, symbol.symbol, user.login, user.kontostand FROM auftrag
+	JOIN aktie
+	ON auftrag.fk_aktie = aktie.aktie_id
+	JOIN symbol
+	ON aktie.fk_symbol = symbol.symbol_id
+	JOIN user
+	ON aktie.fk_user = user.user_id;
+
+	------Statement für portfolio anzeigen---------
+
+	SELECT symbol.symbol, aktie.name, aktie.dividende, user.login FROM aktie
+	JOIN symbol
+	ON aktie.fk_symbol = symbol.symbol_id
+	JOIN user
+	ON aktie.fk_user = user.user_id
+	WHERE user.login ="gwen";
 
 	void top() throws Exception {
 		ConnectionPooling pooling;
