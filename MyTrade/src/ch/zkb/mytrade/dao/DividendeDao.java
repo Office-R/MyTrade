058311d967254d5d3c53f8d4908d88a553c6e156
robin.frehner <robin.frehner@Santis-SAN20.sanschoolsbsdom.local>
2015-07-01 13:53:21 +0200

    Dividende ausschütten hinzugefügt

--- MyTrade/src/ch/zkb/mytrade/dao/DividendeDao.java
+++ MyTrade/src/ch/zkb/mytrade/dao/DividendeDao.java
@@ -17,11 +17,12 @@ import com.mysql.jdbc.PreparedStatement;
 
 public class DividendeDao {
 	
+	private Double dividendeInChFPerStk;
 	public DividendeDao() {
-		// TODO Auto-generated constructor stub
+		System.out.println("DividendeDao");
 	}
 	
-	public synchronized ArrayList<SymbolModel> loadSymbol(){
+	public synchronized ArrayList<SymbolModel> loadSymbole(){
 		ConnectionPooling pooling;
 		ArrayList<SymbolModel> symbole = new ArrayList<SymbolModel>();
 		pooling        = ConnectionPoolingImplementation.getInstance(1, 5);
@@ -30,14 +31,14 @@ public class DividendeDao {
 		PreparedStatement prepStmt;
 
 		try {
-			String sqlQuery = "SELECT symbol.symbol_id, symbol.symbol";
+			String sqlQuery = "SELECT symbol.symbol_id, symbol.symbol FROM symbol";
 			prepStmt = (PreparedStatement) c1.prepareStatement(sqlQuery);
 			ResultSet rs = prepStmt.executeQuery();
 			
 			while(rs.next()) {
 				SymbolModel symbolModel = new SymbolModel();
-				symbolModel.setSymbol_id(rs.getInt("symbol.symbol_id"));
-				symbolModel.setSymbol_id(rs.getInt("symbol.symbol"));
+				symbolModel.setSymbol_id(rs.getInt   ("symbol.symbol_id"));
+				symbolModel.setSymbol   (rs.getString("symbol.symbol"));
 				symbole.add(symbolModel);
 			}
 			prepStmt.close();
@@ -48,4 +49,55 @@ public class DividendeDao {
 		pooling.putConnection(c1);
 		return symbole;
 	}
+	
+	public synchronized void ausschuetten(String symbol){
+		System.out.println("Ausschütten: " + symbol);
+		ConnectionPooling pooling;
+		pooling        = ConnectionPoolingImplementation.getInstance(1, 5);
+
+		Connection c1  = pooling.getConnection();
+		PreparedStatement prepStmt;
+		PreparedStatement prepStmtUpdate;
+
+		try {
+			String sqlQuery = " SELECT aktie.fk_user from aktie"
+							+ " JOIN symbol"
+							+ " ON aktie.fk_symbol  = symbol.symbol_id"
+							+ " Where symbol.symbol = ?";
+			prepStmt = (PreparedStatement) c1.prepareStatement(sqlQuery);
+			prepStmt.setString(1, symbol);
+			ResultSet rs = prepStmt.executeQuery();
+			System.out.println(getDividendeInChFPerStk());
+			while(rs.next()) {
+				String sqlUpdate = " UPDATE user"
+					           	+  " SET user.kontostand = user.kontostand + ?"
+					           	+  " WHERE user.user_id = ?";
+				prepStmtUpdate = (PreparedStatement) c1.prepareStatement(sqlUpdate);
+				prepStmtUpdate.setDouble(1, getDividendeInChFPerStk());
+				prepStmtUpdate.setInt(2, rs.getInt("aktie.fk_user"));
+				int returnValue = prepStmtUpdate.executeUpdate(); // return value abgefangen 
+																  // ohne gabs eine sql-Exception
+				System.out.println("Dividende für user Id: "  + rs.getInt("aktie.fk_user"));
+			}
+			prepStmt.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		pooling.putConnection(c1);
+	}
+
+	public Double getDividendeInChFPerStk() {
+		return dividendeInChFPerStk;
+	}
+
+	public void setDividendeInChFPerStk(Double dividendeInChFPerStk) {
+		if(dividendeInChFPerStk != null){
+			this.dividendeInChFPerStk = dividendeInChFPerStk;
+		}
+	}
+
+	
+	
+	
 }
