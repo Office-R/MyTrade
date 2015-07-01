93f9c5c6c5ccfdf62fea93cb94667ffc33842e05
gwendolin.maggion <gwendolin.maggion@Santis-SAN29.sanschoolsbsdom.local>
2015-07-01 14:09:17 +0200

    neuer admin filter

---
+++ MyTrade/src/ch/zkb/mytrade/controller/AdminFilter.java
@@ -0,0 +1,110 @@
+package ch.zkb.mytrade.controller;
+
+import java.io.IOException;
+
+import javax.faces.context.FacesContext;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import ch.zkb.mytrade.model.Rolle;
+import ch.zkb.mytrade.model.UserModel;
+
+@WebFilter("/faces/private/admin/*")
+public class AdminFilter implements Filter {
+
+	
+	boolean debug = true;
+	private void debugOut(String meldung) {
+		if(debug) {
+			System.out.println("Debug MyAuthFilter." + meldung);
+		}
+	}
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		debugOut("init(): AuthFilter...");
+	}
+
+/**
+ * Versuche, die Session aus dem Request zu holen.
+ * Ist das nicht möglich, so gehe über den FacesContext.
+ */
+	HttpSession holeSessionVariable(HttpServletRequest request) {
+		
+		HttpSession session = request.getSession(false);
+		if(null == session) {
+			FacesContext facesContext = FacesContext.getCurrentInstance();
+			if(null == facesContext || null == facesContext.getExternalContext()) {
+				debugOut("holeSessionVariable(): No session!");
+			} else {
+				session = (HttpSession) facesContext.getExternalContext().getSession(true);            		
+			}
+		}
+		return session;
+	}
+
+
+	String meinPortfolioUrl = "http://localhost:8080/MyTrade/faces/private/mein_portfolio.xhtml";
+	String loginUrl = "http://localhost:8080/MyTrade/faces/login.xhtml";
+
+
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res,
+			FilterChain chain) throws IOException, ServletException {
+		try {
+			debugOut("doFilter(): start...");
+
+			eigenerDoHTTPFilter((HttpServletRequest)  req, 
+			                    (HttpServletResponse) res, 
+			                    chain);	
+
+			debugOut("doFilter(): ... done.TraderoderAdmin");
+		} catch (Exception ex) {
+			System.out.println("Exception im MyAuthFilter " + ex);
+			ex.printStackTrace();
+		}
+	}
+
+
+	/**
+	 * Wie "doFilter", doch a) mit throws, statt try-catch und
+	 *                      b) mit HttpServlet, statt Servlet
+	 */
+	private void eigenerDoHTTPFilter(HttpServletRequest request, HttpServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		
+		UserModel user = (UserModel) holeSessionVariable(request).getAttribute("currentUser");
+		
+		if(null == user){
+			debugOut("eigenerDoHTTPFilter(): Trader ist angemeldet");
+			response.sendRedirect(loginUrl);
+			return;
+		}
+		
+		if(user.getRolle() == Rolle.ADMINISTRATOR) {
+			debugOut("eigenerDoHTTPFilter(): Administrator ist angemeldet");
+			chain.doFilter(request, response); // jeder, da öffentlich	
+			return;
+		}
+		if(user.getRolle() == Rolle.TRADER) {
+			debugOut("eigenerDoHTTPFilter(): Trader ist angemeldet");
+			response.sendRedirect(meinPortfolioUrl);
+			return;
+		}
+
+	}
+
+
+	@Override
+	public void destroy() {
+		// must be overriden
+	}
+}
