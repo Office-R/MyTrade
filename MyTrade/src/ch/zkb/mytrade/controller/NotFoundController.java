e1b6e7bf2d6e73cdf22f6cc3bd5d897e1100578e
robin.frehner <robin.frehner@Santis-SAN20.sanschoolsbsdom.local>
2015-07-02 16:22:38 +0200

    sql angepasst

---
+++ MyTrade/src/ch/zkb/mytrade/controller/NotFoundController.java
@@ -0,0 +1,12 @@
+package ch.zkb.mytrade.controller;
+
+public class NotFoundController {
+	public NotFoundController() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String sendBack(){
+		System.out.println("joaaa");
+		return "mein_portfolio?faces-redirect=true";
+	}
+}
